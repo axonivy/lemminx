6bc8cb6092fdad36da35a5facdfc7a1e25273bcc
ivy-jh <jens.hartmann@axonivy.com>
2025-10-14 11:29:04 +0200

    wip

--- org.eclipse.lemminx/src/main/java/org/eclipse/lemminx/utils/MarkdownConverter.java
+++ org.eclipse.lemminx/src/main/java/org/eclipse/lemminx/utils/MarkdownConverter.java
@@ -3,13 +3,11 @@ package org.eclipse.lemminx.utils;
 import com.vladsch.flexmark.html2md.converter.*;
 import com.vladsch.flexmark.util.data.MutableDataSet;
 
-import static org.apache.commons.text.StringEscapeUtils.unescapeJava;
 import static org.apache.commons.text.StringEscapeUtils.unescapeXml;
 
 public class MarkdownConverter {
 
 	public static String convert(String html) {
-
 		if (!StringUtils.isTagOutsideOfBackticks(html)) {
 			return unescapeXml(html);
 		}
@@ -18,6 +16,6 @@ public class MarkdownConverter {
 		options.set(FlexmarkHtmlConverter.MAX_BLANK_LINES, 1);
 
 		var converter = FlexmarkHtmlConverter.builder(options).build();
-		return unescapeJava(converter.convert(html, -1));
+		return converter.convert(html, -1);
 	}
 }
