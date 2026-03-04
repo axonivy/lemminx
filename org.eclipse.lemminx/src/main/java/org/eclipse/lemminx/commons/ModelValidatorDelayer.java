214fd8be84555a51c5d77f0614ff2fa77f4867d3
Louis <louis.mueller@axonivy.com>
2026-03-04 15:04:21 +0100

    set newScheduledThreadPool size 0 (#34)

--- org.eclipse.lemminx/src/main/java/org/eclipse/lemminx/commons/ModelValidatorDelayer.java
+++ org.eclipse.lemminx/src/main/java/org/eclipse/lemminx/commons/ModelValidatorDelayer.java
@@ -39,7 +39,7 @@ public class ModelValidatorDelayer<T> {
 	private final long validationDelayMs;
 
 	public ModelValidatorDelayer(Consumer<ModelTextDocument<T>> validator) {
-		this(Executors.newScheduledThreadPool(2, Thread.ofVirtual().factory()), validator, DEFAULT_VALIDATION_DELAY_MS);
+		this(Executors.newScheduledThreadPool(0, Thread.ofVirtual().factory()), validator, DEFAULT_VALIDATION_DELAY_MS);
 	}
 
 	public ModelValidatorDelayer(ScheduledExecutorService executorService, Consumer<ModelTextDocument<T>> validator,
